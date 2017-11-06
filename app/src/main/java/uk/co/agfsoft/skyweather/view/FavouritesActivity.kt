4cf01e0f239ee7d4e7cc9dd8d1a7e696287df5e2
Drew Ferrier <agferrier@btinternet.com>
2017-11-06 22:48:31 +0000

    Initial conversion to kotlin for FavouritesActivity

---
+++ app/src/main/java/uk/co/agfsoft/skyweather/view/FavouritesActivity.kt
@@ -0,0 +1,124 @@
+package uk.co.agfsoft.skyweather.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import butterknife.ButterKnife
+import butterknife.OnClick
+import kotlinx.android.synthetic.main.activity_favourites.*
+import kotlinx.android.synthetic.main.content_favourites.*
+import uk.co.agfsoft.skyweather.R
+import uk.co.agfsoft.skyweather.dagger.ActivityComponent
+import uk.co.agfsoft.skyweather.dagger.ActivityModule
+import uk.co.agfsoft.skyweather.dagger.InjectableActivity
+import uk.co.agfsoft.skyweather.dagger.InjectableApplication
+import uk.co.agfsoft.skyweather.model.WeatherCity
+import uk.co.agfsoft.skyweather.presenter.FavouritesPresenter
+import uk.co.agfsoft.skyweather.view.adapter.FavouriteAdapter
+import javax.inject.Inject
+
+class FavouritesActivity : AppCompatActivity(), InjectableActivity, FavouritesView {
+
+
+    lateinit private var activityComponent: ActivityComponent
+    lateinit private var favouriteAdapter: FavouriteAdapter
+
+    @Inject
+    lateinit internal var favouritesPresenter: FavouritesPresenter
+
+    //<editor-fold desc="Lifecycle methods">
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getActivityComponent().inject(this)
+
+        setContentView(R.layout.activity_favourites)
+        ButterKnife.bind(this)
+        setSupportActionBar(toolbar)
+        initialiseRecyclerView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        favouritesPresenter.viewReady()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_favourites, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+    //</editor-fold>
+
+
+    //<editor-fold desc="Event handlers">
+    @OnClick(R.id.fab)
+    fun onFabClicked(view: View) {
+        favouritesPresenter.addFavouriteClicked()
+    }
+    //</editor-fold>
+
+
+    //<editor-fold desc="FavouritesView implementation">
+    override fun showAddFavouriteView() {
+        val intent = Intent(this, AddFavouriteActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun showIntroOverlay(showView: Boolean) {
+        introOverlay.visibility = if (showView) View.VISIBLE else View.GONE
+    }
+
+    override fun updateFavourites(favourites: List<WeatherCity>) {
+        favouriteAdapter.setFavourites(favourites)
+        favouriteAdapter.notifyDataSetChanged()
+    }
+
+    override fun showWeatherForCity(weatherCity: WeatherCity) {
+        val intent = Intent(this, ShowFavouriteActivity::class.java)
+        intent.putExtra(ShowFavouriteActivity.CITY_ID, weatherCity.id)
+        intent.putExtra(ShowFavouriteActivity.CITY_NAME, weatherCity.name)
+        intent.putExtra(ShowFavouriteActivity.CITY_COUNTRY, weatherCity.country)
+
+        startActivity(intent)
+    }
+    //</editor-fold>
+
+
+    //<editor-fold desc="InjectableActivity implementation">
+    override fun getActivityComponent(): ActivityComponent {
+        activityComponent = (application as InjectableApplication)
+                .applicationComponent
+                .plus(ActivityModule(this))
+        return activityComponent
+
+    }
+    //</editor-fold>
+
+
+    //<editor-fold desc="Service routines">
+    private fun initialiseRecyclerView() {
+        favouritesRecyclerView!!.layoutManager = LinearLayoutManager(this)
+        favouriteAdapter = FavouriteAdapter(favouritesPresenter.favourites, favouritesPresenter)
+        favouritesRecyclerView!!.adapter = favouriteAdapter
+    }
+    //</editor-fold>
+
+
+}
